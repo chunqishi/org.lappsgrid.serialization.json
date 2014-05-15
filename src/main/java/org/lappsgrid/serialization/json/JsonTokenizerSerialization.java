c503dcd9e799b7d94323a2de72d7bb4b9455920e
chunqishi <shicq@cs.brandeis.edu>
2014-05-15 11:11:04 -0400

    remote unused code

--- src/main/java/org/lappsgrid/serialization/json/JsonTokenizerSerialization.java
+++ src/main/java/org/lappsgrid/serialization/json/JsonTokenizerSerialization.java
@@ -8,22 +8,6 @@ import org.lappsgrid.vocabulary.Annotations;
  * Created by shi on 5/13/14.
  */
 public class JsonTokenizerSerialization extends JsonSerialization {
-    JSONObject json = null;
-    JSONObject currentStep = null;
-    JSONObject currentStepMeta = null;
-    JSONObject contains = null;
-    JSONObject contain = null;
-    JSONArray annotations = null;
-    JSONArray steps = null;
-    JSONObject text = null;
-    JSONObject metadata = null;
-    static int id = 0;
-
-    JSONObject lastStep = null;
-    JSONObject lastStepMeta = null;
-    JSONArray lastStepAnnotations = null;
-    JSONObject lastStepContains = null;
-
     public JsonTokenizerSerialization(String textjson) {
         super(textjson);
         this.setAnnotationType(Annotations.TOKEN);
